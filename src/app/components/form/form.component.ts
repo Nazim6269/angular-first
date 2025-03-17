e62a13c7b8ba1c9ddb08242c9bcfc487ace511e0
nazimuddin <nazimdev10022001@gmail.com>
2025-03-17 16:40:13 +0600

    added some validation and best practices

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,28 +1,71 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// Custom validator function to check forbidden names
+export function forbiddenNameValidator(forbiddenPattern: RegExp) {
+  return (control: FormControl) => {
+    return forbiddenPattern.test(control.value)
+      ? { forbiddenName: true }
+      : null;
+  };
+}
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf, NgClass],
 })
 export class FormComponent {
-  registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          forbiddenNameValidator(/bobs/i),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(8),
+          Validators.pattern(/^[1-9]/),
+        ],
+      ],
+    });
+  }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
 
   onSubmit() {
-    console.warn(this.registerForm.value);
+    console.log(this.registerForm.value);
   }
 
   updateName() {
-    this.registerForm.setValue({
-      name: '',
-      email: '',
-      password: '',
-    });
+    this.registerForm.reset();
   }
 }
